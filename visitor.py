b1967629f6cd5ed492b83b0818fc8e27c834acad
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-09-28 23:58:25 +0300

    Use Generic[T]
    T = TypeVar("T")
    
    class Visitor(Generic[T]):

--- visitor.py
+++ visitor.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-from typing import Any
+from typing import NoReturn, TypeVar, Generic
 import node as N
 
+T = TypeVar("T")
 
-class Visitor:
+
+class Visitor(Generic[T]):
     def _get_method_name(self, node: N.Node) -> str:
         return f"visit_{type(node).__name__.lower()}"
 
-    def visit(self, node: N.Node) -> str:
+    def visit(self, node: N.Node) -> T:
         meth = getattr(self, self._get_method_name(node), self.visit_generic)
         return meth(node)
 
-    def visit_generic(self, node: N.Node) -> str:
+    def visit_generic(self, node: N.Node) -> T:
         raise TypeError(f"No method {self._get_method_name(node)} to visit {node}")
 
 
-class PrefixNotation(Visitor):
+class PrefixNotation(Visitor[str]):
     def visit_number(self, node: N.Number) -> str:
         return f"{str(node.value)}"
 
