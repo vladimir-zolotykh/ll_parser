0b4c40f2a8f7d4a411fe518f3a8d3aa725c8e107
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-09-28 19:27:19 +0300

    Initial import
    Classes Number, Add, Div, etc.

---
+++ node.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+
+class Node:
+    pass
+
+
+class UnaryOperator(Node):
+    def __init__(self, operand):
+        self.operand = operand
+
+
+class BinaryOperator(Node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class Number(Node):
+    def __init__(self, val):
+        self.val = val
+
+
+class Add(BinaryOperator):
+    pass
+
+
+class Sub(BinaryOperator):
+    pass
+
+
+class Mul(BinaryOperator):
+    pass
+
+
+class Div(BinaryOperator):
+    pass
+
+
+class Negate(UnaryOperator):
+    pass
